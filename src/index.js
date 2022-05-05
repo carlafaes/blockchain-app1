7f1200902566b6e68f6ed240d2e6ac31d235ba32
carlafaes <carlafaes18@hotmail.com>
2022-05-05 17:24:44 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -33,8 +33,15 @@ app.use(express.json())
 app.use(cors())
 
 //routes
-app.get('/', (req,res) => {
-    res.send('Welcome')
+app.get('/', (req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
 });
 
 //mongodb connection
