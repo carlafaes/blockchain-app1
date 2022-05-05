5cc02c5ee99c2c2143709736ff5fcd633d49c46b
carlafaes <carlafaes18@hotmail.com>
2022-05-05 01:48:08 -0300

    schema

---
+++ src/models/adress.js
@@ -0,0 +1,19 @@
+const mongoose= require('mongoose');
+const { Schema }= require('mongoose');
+
+const adressSchema = new mongoose.Schema({
+    adress:{
+        type:String,
+        required: true,
+    },
+    value:{
+        type:String,
+        required: true,
+    },
+    transactionIndex:{
+        type: String,
+        required:true,
+    }
+});
+
+module.exports = mongoose.model('User', adressSchema)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const axios= require('axios');
+const { API_KEY} = process.env;
+const adressSchema = require('../models/adress');
 
-//create user
-router.post('/user', (req, res)=>{
-    res.send('create user')
+
+//get info
+router.get('/wallet', async (req, res)=>{
+    try{
+        let info = await axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=${API_KEY}`)
+        let response = info.data.result
+        res.json(response)
+    }
+    catch(error){
+        console.log(error,'error en el get')
+    }
+
+})
+
+router.post('/add',(req, res)=>{
+    adressSchema(req.body);
 })
 
 module.exports = router
