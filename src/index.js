cc409c2d3486d6f69b191ef1716f68b999f42bdb
carlafaes <carlafaes18@hotmail.com>
2022-05-04 21:04:14 -0300

    ENV

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+const { MONGODB_PASS, MONGODB_USER} = process.env;
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//routes
+app.get('/', (req,res) => {
+    res.send('Welcome')
+});
+
+//mongodb connection
+mongoose
+.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.idcfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((error) => console.log(error));
 
 app.listen(port,() => console.log('server listening on port ', port));
