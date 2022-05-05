a44f9116858011f17d0a14f3ae04de554fa40f25
carlafaes <carlafaes18@hotmail.com>
2022-05-05 13:08:05 -0300

    concat

--- src/controllers/adress.js
+++ src/controllers/adress.js
@@ -6,8 +6,11 @@ const { API_KEY} = process.env;
 exports.getInfo= async (req, res)=>{
     try{
         let info = await axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=${API_KEY}`)
-        let response = info.data.result
-        res.json(response)
+        let transacciones= await axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${API_KEY}`)
+        let response = transacciones.data.result
+        let response_dos = info.data.result
+        let concat= [...response, response_dos]
+        res.json(concat)
     }
     catch(error){
         console.log(error,'error en el get')
@@ -16,7 +19,7 @@ exports.getInfo= async (req, res)=>{
 }
 exports.blockTimeNumber = async(req, res) =>{
     try{
-        let number= await axios.get(`https://api.etherscan.io/api?module=stats&action=chainsize&startdate=2019-02-01&enddate=2019-02-28&clienttype=geth&syncmode=default&offset=10&sort=asc&apikey=${API_KEY}`)
+        let number= await axios.get(`https://api.etherscan.io/api?module=stats&action=chainsize&startdate=2022-04-13&enddate=2022-05-05&clienttype=geth&syncmode=default&offset=10&sort=asc&apikey=${API_KEY}`)
         let response = number.data.result
         //console.log(response)
         res.json(response);
