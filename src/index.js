be06acd792842ed1ba0e5098d837ca16a6de035d
carlafaes <carlafaes18@hotmail.com>
2022-05-05 22:01:29 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use(express.json())
 app.use(cors())
 
 //routes
-app.get('/', (req,res,next) => {
+app.use('/', (req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
