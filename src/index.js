8b4f96eec0bd47523954b64e70f7ea4d93c299e7
carlafaes <carlafaes18@hotmail.com>
2022-05-06 10:21:10 -0300

    cors solution

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
 const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 const userRoutes = require('./routes/user')
 
 const { MONGODB_PASS, MONGODB_USER, DATABASE,CORS} = process.env;
 
-const app = express();
-const port = process.env.PORT || 3000;
 
 //for parsing json
 app.use(
@@ -30,7 +38,6 @@ app.use(
 //middleware
 app.use('/api', userRoutes)
 app.use(express.json())
-app.use(cors())
 
 
 //mongodb connection
@@ -41,13 +48,13 @@ mongoose
 app.listen(port,() => console.log('server listening on port ', port));
 
 //routes
-app.use('/', (req,res,next) => {
+app.use('/', (req,res) => {
     res.header("Access-Control-Allow-Origin", `${CORS}`); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", true);
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
+    
 });
